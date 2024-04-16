fdbbe210181c67ab0abe373e9fda552be9a45beb
jomorodi <omorojeff@gmail.com>
2024-04-16 12:21:01 +0300

    created test user and test items and registering at the admin site

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -4,3 +4,5 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Profile)
+admin.site.register(Item)
+admin.site.register(Transaction)
--- backend/base/views.py
+++ backend/base/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from base.models import Item
 from django.http import HttpResponse , JsonResponse
+from django.views import generic
+import random
+
+MAX_ITEM_PRICE = 100
+MIN_ITEM_PRICE = 0
 
 # Create your views here.
 
@@ -12,27 +17,6 @@ def homePage (request):
     return render(request, "base/index.html", context)
 
 
-def populate(request):
-    # clearing the DB
-    User.objects.all().delete()
-    Item.objects.all().delete()
-
-    #populating with no_u users and no_c cards each
-    no_u = 3
-    no_c = 2
-    try:
-        color_list = ['red', 'green', 'blue', 'pink', 'black', 'orange']
-        for n in range(no_u):
-            user = User.objects.create_user("user{}".format(n), "user{}@myserver.com".format(n), "pass{}".format(n))
-            user.save()
-
-            for i in range(no_c):
-                item = Card(color=random.choice(color_list), owner=user)
-                item.save()
-        message = "populated with {} users and {} cards each".format(no_u, no_c)
-    except Exception as e:
-        message = "Populate failed:  " + str(e)
-    return JsonResponse({"message": message})
 
 
 def generate_test_users(request):
@@ -46,7 +30,22 @@ def generate_test_users(request):
             email = f'{username}@shop.aa'
             password = f'pass{i}'
             user = User.objects.create_user(username=username, email=email, password=password)
+            
+            if i <= 3 :
+                 title = f'title{i}'
+                 description = f'description{i}'
+                 price = random.randint(MIN_ITEM_PRICE , MAX_ITEM_PRICE )
+                 item = Item.objects.create(title=title , description=description , price=price , seller=user)
+                 
         
         return HttpResponse("Test users successfully created.")
     except Exception as e:
         return HttpResponse(f"Failed to create test users: {str(e)}", status=500)
+
+
+class ItemListView(generic.ListView):
+    """Generic class-based view for a list of books."""
+    model = Item
+    paginate_by = 10
+
+
