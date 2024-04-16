20064b668f6c62a0603a79cc59778c89d9ba422f
jomorodi <omorojeff@gmail.com>
2024-04-16 19:39:28 +0300

    changed the redirect url

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -8,3 +8,10 @@ urlpatterns = [
     path('item/<int:pk>', views.ItemDetailView.as_view(), name='item-detail'),
     
 ]
+
+urlpatterns += [
+    path('item/create/', views.ItemCreate.as_view(), name='item-create'),
+ 
+]
+
+#   path('item/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),   path('item/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
