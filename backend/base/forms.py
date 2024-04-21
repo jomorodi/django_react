e7d9d0687d36e81a152ccc88805c0b70cb8756ae
jomorodi <omorojeff@gmail.com>
2024-04-22 00:42:47 +0300

    minor changes in the form file

--- backend/base/forms.py
+++ backend/base/forms.py
@@ -1,16 +1,12 @@
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 import datetime  # for checking renewal date range.
-
 from django import forms
+from .models import Item
 
 
 class CreateItemForm (forms.Form):
     pass
-
-
-from django import forms
-from .models import Item
 
 class ItemPriceEditForm(forms.ModelForm):
     class Meta:
